1aebf9edec8ea5a7198400568189b14c98e3c198
d-rocco <dhrocco27@gmail.com>
2023-01-16 13:56:05 -0500

    Added functions to add and remove grid
    The functions react to what the user inputs. If the user inputs an invalid input, there is a popup alert that advises the user to make a valid input. I also made some CSS and HTML changes for a better looking website.

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 let selected_square_count = 0;
+let grid_container = document.querySelector('.grid-container');
 
 const grid_count_btn = document.querySelector('.grid-count-btn');
 grid_count_btn.addEventListener('click', () => {
     let input = prompt('Input a number!');
-    if (input > 0 && input <= 100 && input % 1 === 0)
+    if (input > 1 && input <= 100 && input % 1 === 0)
         selected_square_count = input;
     else {
-        alert('Seriously though, pick a number between 0 and 100.')
+        alert('Seriously though, pick a number between 2 and 100.')
         selected_square_count = 0;
     }
+    removeGrid();
+    createGrid(selected_square_count);
 });
+
+function removeGrid() {
+    while (grid_container.firstChild) {
+        grid_container.removeChild(grid_container.firstChild)
+    }
+}
+
+function createGrid(selected_square_count) {
+    let grid_block_size = (960.0 / selected_square_count) - 2;
+    for (let i = 0; i < selected_square_count; i++) {
+        let row = document.createElement('div');
+        for (let j = 0; j < selected_square_count; j++) {
+            let grid_block = document.createElement('div');
+            grid_block.classList.add('grid-block');
+            row.appendChild(grid_block);
+        }
+        row.classList.add('row');
+        grid_container.appendChild(row);
+    }
+}
